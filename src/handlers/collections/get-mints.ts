84234e4c78f60de527f562b79c79665cc7a04f10
Joe Fitter <hello@joefitter.com>
2022-12-19 15:47:02 +0100

    update to include filter by collection

--- src/handlers/collections/get-mints.ts
+++ src/handlers/collections/get-mints.ts
@@ -3,10 +3,10 @@ import { getMints } from '../../db';
 
 export async function handleGetMints(req: Request, res: Response, next: NextFunction) {
   const { collection } = req.params;
-  const { limit, page, orderBy, publicKey }: { limit?: string, page?: string, orderBy?: string, publicKey?: string } = req.query;
+  const { limit, page, orderBy, publicKey, collectionFilter }: { limit?: string, page?: string, orderBy?: string, publicKey?: string, collectionFilter?: string } = req.query;
   try {
     const mints = await getMints({
-      collection,
+      collection: collection || collectionFilter,
       limit: limit ? parseInt(limit, 10) : undefined,
       page: page ? parseInt(page, 10) : undefined,
       orderBy,
